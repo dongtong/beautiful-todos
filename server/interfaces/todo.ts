ec36ee58f38056990d23671f257418c27c42d383
Henry <djh@jinhus-MacBook-Pro.local>
2024-02-02 13:11:26 +0800

    Define todo interface

---
+++ server/interfaces/todo.ts
@@ -0,0 +1,20 @@
+/**
+ * Define TODO interface
+ * @columns
+ * id: [string] primary key
+ * title: [string] todo's title
+ * status: [string] todo's status, enum value: New and Completed
+ * createdAt: [string] todo's created timestamp
+ * lastUpdatedAt: [string] todo's lastest updated timestamp
+ * completedAt: [string] todo's completed timestamp
+ * creator: [string][optional] system could persist todo's creator name if necessary
+ */
+export interface ITodo {
+  id: string;
+  title: string;
+  status: "New" | "Completed";
+  createdAt: string;
+  lastUpdatedAt: string;
+  completedAt: string;
+  creator?: string;
+}
