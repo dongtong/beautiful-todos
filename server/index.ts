8b36838616289f842b19b08af4f54b5e04195a19
Henry <djh@jinhus-MacBook-Pro.local>
2024-02-03 19:26:42 +0800

    error log and handler

--- server/index.ts
+++ server/index.ts
@@ -2,14 +2,31 @@ import express from "express";
 import commonRouter from "./controllers/common";
 import todoRouter from "./controllers/todos";
 import bodyParser from "body-parser";
+import {
+  requestLogger,
+  errorHandler,
+  errorLogger,
+  invalidPathHandler,
+} from "lib/common";
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(requestLogger);
+
 app.use("/common", commonRouter);
 app.use("/todos", todoRouter);
 
-// catch global exception
+// Step 1: Attach the first Error handling Middleware
+// function defined above (which logs the error)
+app.use(errorLogger);
+
+// Step 2: catch error and response client
+app.use(errorHandler);
+
+// Attach the fallback Middleware
+// function which sends back the response for invalid paths)
+app.use(invalidPathHandler);
 
 app.listen(8000, () =>
   console.log("Beautiful todos server is running on port: 8000")
--- server/lib/common.ts
+++ server/lib/common.ts
@@ -1,5 +1,7 @@
+import { Request, Response, NextFunction } from "express";
 import { Prisma } from "lib/prisma";
 import { IApiRes } from "interfaces/common";
+import { AppError } from "lib/appError";
 
 export function catchORMError(defaultMsg: string, err?: unknown): IApiRes {
   // type narrowing
@@ -14,4 +16,48 @@ export function catchORMError(defaultMsg: string, err?: unknown): IApiRes {
     statusCode: "ERROR",
     message: defaultMsg,
   };
+}
+
+export function requestLogger(req: Request, res: Response, next: NextFunction) {
+  console.log(`${req.method} - ${req.url}`);
+  next();
+}
+
+// Error handling Middleware function for logging the error message
+export function errorLogger(
+  error: AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.log(`${req.method} url(${req.url}) -- error ${error.message}`);
+  // calling next middleware
+  next(error);
+}
+
+export function errorHandler(
+  error: AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  res.header("Content-Type", "application/json");
+
+  return res.send({
+    statusCode: `${error.statusCode}` || "ERROR",
+    message: error.message || "Server internal error, please contact support.",
+  });
+}
+
+// Fallback Middleware function for returning
+// 404 error for undefined paths
+export function invalidPathHandler(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  return res.send({
+    statusCode: 404,
+    message: "Invalid path",
+  });
 }
